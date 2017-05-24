6d3ba915ca00b6d5fb4560abd341de7073a67a81
nickweseman <trumpernickel2@yahoo.com>
2017-05-24 00:17:52 -0500

    added maven repositories

--- src/Valid.java
+++ src/Valid.java
@@ -1,7 +1,14 @@
+import com.google.common.collect.Multimap;
+import com.google.gson.Gson;
+
 public class Valid {
 
     public static void main(String[] args) {
 
         System.out.println("This class is valid");
+
+        Multimap map;
+
+        Gson gson;
     }
 }
